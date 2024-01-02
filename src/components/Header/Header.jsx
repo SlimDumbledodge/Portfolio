89803b569e2864997a9149ab9c224a62afe60ee7
Amael Rosales <amael.rosales@gmail.com>
2024-01-02 19:08:08 +0100

    BUGFIX - Navbar not closing after clicking on link, this is fix now

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,40 +1,57 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { useState } from 'react';
 import './Header.scss';
 
 function Header() {
+  const [isNavbarOpen, setNavbarOpen] = useState(true);
+  const handleClick = () => {
+    setNavbarOpen((current) => !current);
+  };
+
+  const openNavbar = {
+    left: 0,
+    transition: 'all 0.5s',
+  };
+
+  const closeNavbar = {
+    left: '-100%',
+    transition: 'all 0.5s',
+  };
   return (
     <nav className="navbar">
       <a href="/">
         <strong className="logo">Amael.dev</strong>
       </a>
-
-      <input type="checkbox" id="check" />
+      <input type="checkbox" id="check" onChange={handleClick} />
       <label htmlFor="check" className="navbar__burguer__menu">
         <i className="fa-solid fa-bars" id="burger__icon" />
       </label>
-      <ul className="navbar__ul">
+      <ul
+        className="navbar__ul"
+        style={isNavbarOpen ? openNavbar : closeNavbar}
+      >
         <li className="navbar__li">
-          <a className="navbar__a" href="#home">
+          <a className="navbar__a" href="#home" onClick={handleClick}>
             Accueil
           </a>
         </li>
         <li className="navbar__li">
-          <a className="navbar__a" href="#about">
+          <a className="navbar__a" href="#about" onClick={handleClick}>
             A propos
           </a>
         </li>
         <li className="navbar__li">
-          <a className="navbar__a" href="#projects">
+          <a className="navbar__a" href="#projects" onClick={handleClick}>
             Projets
           </a>
         </li>
         <li className="navbar__li">
-          <a className="navbar__a" href="#skills">
+          <a className="navbar__a" href="#skills" onClick={handleClick}>
             Compétences
           </a>
         </li>
         <li className="navbar__li">
-          <a className="navbar__a" href="#contact">
+          <a className="navbar__a" href="#contact" onClick={handleClick}>
             Contact
           </a>
         </li>
@@ -44,6 +61,7 @@ function Header() {
             href="https://www.dropbox.com/scl/fi/4voysqkgyte79qnw0as1s/CV.pdf?rlkey=w7wri6iywpcynf6yf687fn9uh&dl=0"
             rel="noreferrer"
             target="_blank"
+            onClick={handleClick}
           >
             CV
           </a>
